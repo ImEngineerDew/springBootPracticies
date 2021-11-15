53eafa3b47428e93723f535a50f516e23c1db5b6
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-11-15 13:04:11 -0500

    Creating the Student model and the course model

---
+++ StudentRelated/src/main/java/com/toadsdewin/StudentRelated/models/CourseModel.java
@@ -0,0 +1,39 @@
+package com.toadsdewin.StudentRelated.models;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class CourseModel
+{
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (unique = true , nullable = false)
+
+    private Long id;
+    private String name;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ StudentRelated/src/main/java/com/toadsdewin/StudentRelated/models/StudentModel.java
@@ -0,0 +1,6 @@
+package com.toadsdewin.StudentRelated.models;
+
+public class StudentModel
+{
+
+}
