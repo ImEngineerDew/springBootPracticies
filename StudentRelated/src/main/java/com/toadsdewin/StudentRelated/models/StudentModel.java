fc4273d2d776727d8c01db78e029d1b3c8ce4699
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-11-15 13:13:33 -0500

    Creating the getters and setters from the Student model

--- StudentRelated/src/main/java/com/toadsdewin/StudentRelated/models/StudentModel.java
+++ StudentRelated/src/main/java/com/toadsdewin/StudentRelated/models/StudentModel.java
@@ -1,6 +1,48 @@
 package com.toadsdewin.StudentRelated.models;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "students")
 public class StudentModel
 {
+     @Id
+     @Column
+     private Long id;
+     private String name;
+     private String surname;
 
+     @ManyToOne
+     private CourseModel course;
+
+     public Long getId() {
+          return id;
+     }
+
+     public void setId(Long id) {
+          this.id = id;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public void setName(String name) {
+          this.name = name;
+     }
+
+     public String getSurname() {
+          return surname;
+     }
+
+     public void setSurname(String surname) {
+          this.surname = surname;
+     }
+
+     public CourseModel getCourse() {
+          return course;
+     }
+
+     public void setCourse(CourseModel course) {
+          this.course = course;
+     }
 }
